a6215313484cd61205a91a115ca990d49f3f7cf6
anusngh1986 <anusngh1986@gmail.com>
2023-09-18 01:36:39 +0530

    @Anurag

--- src/main/java/com/api/parallelBatchApiProcess.java
+++ src/main/java/com/api/parallelBatchApiProcess.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultipleAPICallApplication {
+public class parallelBatchApiProcess {
 
     public static void main(String[] args) {
-        SpringApplication.run(MultipleAPICallApplication.class, args);
+        SpringApplication.run(parallelBatchApiProcess.class, args);
     }
 
 }
