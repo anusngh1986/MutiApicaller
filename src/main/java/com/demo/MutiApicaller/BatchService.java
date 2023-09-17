03b7a6a5261908c06f5cd28841d248d24743f9ec
anusngh1986 <anusngh1986@gmail.com>
2023-09-18 01:28:16 +0530

    @Anurag

--- src/main/java/com/demo/MutiApicaller/BatchService.java
+++ src/main/java/com/demo/MutiApicaller/BatchService.java
@@ -3,16 +3,15 @@ package com.demo.MutiApicaller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-
 @Component
 // Batch service will execute batches
 public class BatchService {
 
-@Autowired
-WebClientApiCaller WebClientApiCaller;
+    @Autowired
+    WebClientApiCaller WebClientApiCaller;
     public Flux<Response> callBatch(Batch batch) {
         return Flux.fromIterable(batch.getApis())
-                .flatMap(api -> WebClientApiCaller.callApi(api,Response.class))
+                .flatMap(api -> WebClientApiCaller.callApi(api))
                 .map(response -> new Response(response));
     }
 }
