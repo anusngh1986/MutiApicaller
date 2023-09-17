7e2dc06ac908028c8b7e0a43f11d2b5637773a17
anusngh1986 <anusngh1986@gmail.com>
2023-09-18 01:30:21 +0530

    @Anurag

--- src/main/java/com/demo/MutiApicaller/WebClientApiCaller.java
+++ src/main/java/com/demo/MutiApicaller/WebClientApiCaller.java
@@ -1,8 +1,10 @@
 package com.demo.MutiApicaller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Component
 public class WebClientApiCaller {
 
     private final WebClient webClient;
