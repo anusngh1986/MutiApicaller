d5097252db27675e8ce462356a48f58b1dbd027a
anusngh1986 <anusngh1986@gmail.com>
2023-09-23 23:06:05 +0530

    @Anurag

--- src/main/java/com/api/ParallelBatchApi/MultipleAPICallServiceImpl.java
+++ src/main/java/com/api/ParallelBatchApi/MultipleAPICallServiceImpl.java
@@ -1,5 +1,6 @@
 package com.api.ParallelBatchApi;
 
+
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +8,10 @@ import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
 import static java.util.stream.Collectors.toList;
 
 @Service
@@ -18,8 +19,6 @@ public class MultipleAPICallServiceImpl implements MultipleAPICallService {
 
     private final WebClient webClient;
     private final BatchService batchService;
-    private final int batchSize = 1000;
-    private final int numThreads = 3;
 
     public MultipleAPICallServiceImpl(WebClient webClient, BatchService batchService) {
         this.webClient = webClient;
@@ -35,25 +34,41 @@ public class MultipleAPICallServiceImpl implements MultipleAPICallService {
         apis.add("https://official-joke-api.appspot.com/random_joke");
         apis.add("https://official-joke-api.appspot.com/random_joke");
 
-        // Split the APIs into batches
+
+        // Split the APIs into batches of 1000
         List<Batch> batches = new ArrayList<>();
-        for (int i = 0; i < apis.size(); i += batchSize) {
-            batches.add(new Batch(apis.subList(i, Math.min(i + batchSize, apis.size()))));
+        for (int i = 0; i < apis.size(); i += 1000) {
+            batches.add(new Batch(apis.subList(i, Math.min(i + 1000, apis.size()))));
         }
 
-        // Create a thread pool with a fixed number of threads
-        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+        // Create an executor service to call the batches in parallel
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
-        // Submit tasks to the executor service and collect the futures
-        List<Future<Flux<Response>>> futures = new ArrayList<>(batches.size());
+        // Create a list to store the responses from the batches
+        List<Future<Flux<Response>>> futures = new ArrayList<>();
+
+        // Iterate over the batches and call them in parallel
         for (Batch batch : batches) {
             futures.add(executorService.submit(() -> batchService.callBatch(batch)));
         }
 
-        // Shutdown the executor service after all tasks are completed
+        // Shut down the executor service
         executorService.shutdown();
 
+        // Wait for all the batches to finish calling
+        for (Future<Flux<Response>> future : futures) {
+            future.get();
+        }
+
         // Merge the responses from the batches into a single Flux
-        return Flux.merge(futures.stream().map(Future::get).collect(toList()));
+        return Flux.merge(futures.stream().map(future -> {
+            try {
+                return future.get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).collect(toList()));
     }
 }
